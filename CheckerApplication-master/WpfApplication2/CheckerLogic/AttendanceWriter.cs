6f138edd072a5ec4d22c319f130a7d3c97306344
jonathanmanos <jonathanmanos@gmail.com>
2016-02-02 23:25:37 -0500

    Added no credit feature, hex text, unique text file names

--- CheckerApplication-master/WpfApplication2/CheckerLogic/AttendanceWriter.cs
+++ CheckerApplication-master/WpfApplication2/CheckerLogic/AttendanceWriter.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Management;
 
 namespace WpfApplication2
 {
@@ -15,24 +16,48 @@ namespace WpfApplication2
         private string chapelCheckerID;
         private string eventID;
         private int noCredit;
-        private int hexStudentID;
-        private int hexChapelCheckerID;
-
+        private string hexStudentID;
+        private string hexChapelCheckerID;
+        private string uniqueDeviceID;
 
         public AttendanceWriter()
         {
         }
 
+        private string getUniqueID()
+        {
+            string cpuInfo = string.Empty;
+            ManagementClass mc = new ManagementClass("win32_processor");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            foreach (ManagementObject mo in moc)
+            {
+                cpuInfo = mo.Properties["processorID"].Value.ToString();
+                break;
+            }
+
+            string drive = "C";
+            ManagementObject dsk = new ManagementObject(
+                @"win32_logicaldisk.deviceid=""" + drive + @":""");
+            dsk.Get();
+            string volumeSerial = dsk["VolumeSerialNumber"].ToString();
+
+            string uniqueID = cpuInfo + volumeSerial;
+            this.uniqueDeviceID = uniqueID;
+
+            return uniqueID;
+        }
 
         public void CreateTextFile()
         {
-            this.path = "Attendance_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt";
+            this.path = "Attendance_" + DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + getUniqueID() + ".txt";
             File.Create(path).Close();
 
         }
 
         public void setChapelCheckerID(string id)
         {
+            this.hexChapelCheckerID = Int32.Parse(id, System.Globalization.NumberStyles.HexNumber).ToString();
             this.chapelCheckerID = id;
         }
 
@@ -48,10 +73,11 @@ namespace WpfApplication2
 
         public void WriteTextFile(String s)
         {
+            this.hexStudentID = Int32.Parse(s, System.Globalization.NumberStyles.HexNumber).ToString();
             string dateTime = DateTime.Now.ToString("yyyyMMddHHmmss");
             StreamWriter file = new StreamWriter(path, true);
 
-            file.WriteLine(s + "," + chapelCheckerID + "," + noCredit + "," + eventID + "," + dateTime);
+            file.WriteLine(hexStudentID + "," + hexChapelCheckerID + "," + noCredit + "," + eventID + "," + dateTime);
             file.Close();
             
 
@@ -75,7 +101,7 @@ namespace WpfApplication2
             while ((line = sr.ReadLine()) != null)
             {
 
-                string proxId = line.Substring(0, 4);
+                string proxId = line.Substring(0, 5);
                 proxIdList.Add(proxId);
                 fullLineList.Add(line);
             }
@@ -91,7 +117,11 @@ namespace WpfApplication2
             
             StreamWriter sw = new StreamWriter(tempPath);
 
+            string superLine = "";
             Boolean doesContainId = false;
+            Boolean doesContainNoCredit = false;
+            int noCredit = 0;
+
 
             foreach (string s in proxIdListUnique)
             {
@@ -99,25 +129,45 @@ namespace WpfApplication2
                 //sw.WriteLine(s);
 
                 sr = new StreamReader(path);
+                
 
-                while ((line = sr.ReadLine()) != null && !doesContainId)
+
+
+
+                while ((line = sr.ReadLine()) != null && !doesContainNoCredit)
                 {
 
-                    if (line.Substring(0,4).Contains(s) && !doesContainId)
+                        noCredit = Int32.Parse(line.Substring(12, 1));
+                        //sw.WriteLine(line + "HERE: " + noCredit);
+                    
+                    
+
+                    if (line.Substring(0,5).Contains(s) && !doesContainId && noCredit == 0 && !doesContainNoCredit)
                     {
-                        //sw.WriteLine(line + " contains " + s + "and is the first occurence of" + line);
-                        sw.WriteLine(line);
-                        doesContainId = true;
+                        //sw.WriteLine(line + " contains " + s + "and is the first occurence of" + line + "and is for credit" + noCredit + "and noCredit hasnt arrived" + doesContainNoCredit);
+                        superLine = line;
+                        doesContainId = true;                        
+                    }
+                    else if(line.Substring(0, 5).Contains(s) && noCredit == 1 && !doesContainNoCredit)
+                    {
+                        //sw.WriteLine("OMITTED LINE: " + line + "because it does not contain" + s + " or it is a duplicate" );
+                        superLine = line;
+                        doesContainNoCredit = true;
                     }
                     else
                     {
-                        //sw.WriteLine("OMITTED LINE: " + line + "because it does not contain" + s + " or it is a duplicate");
+
                     }
                     
                 }
+                
 
+                sw.WriteLine(superLine);
                 sr.Close();
+                superLine = "";
                 doesContainId = false;
+                doesContainNoCredit = false;
+                noCredit = 0;
 
             }
 
