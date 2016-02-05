7dfb4164924e8a1831e4af087e91f9ecbfdf9aa5
jonathanmanos <jonathanmanos@gmail.com>
2016-02-05 15:27:43 -0500

    Developing new overall features

---
+++ CheckerApplication-master/WpfApplication2/SQLqueries/SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿select a.validine_id, a.firstname, a.lastname
+  from Attendum.dbo.collectors as c
+  inner join
+  Attendum.dbo.account as a
+  on c.sCollectorAltPin = a.gordon_id
+
---
+++ CheckerApplication-master/WpfApplication2/SQLqueries/SqlQuery_Events.sql
@@ -0,0 +1,5 @@
+﻿ select sEventID, sTitle, dtStart, dtEnd
+ from Attendum.dbo.Events
+ where abs( datediff(day, dtStart, dtEnd) ) <= (31 * 1)
+  and dtStart >=  getdate() -7
+ order by dtStart
